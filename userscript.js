21c447f0b9b9f3e3e200dae987f08c8240fab96a
YumYummity <103061664+YumYummity@users.noreply.github.com>
2023-04-19 13:43:45 -0400

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -6,8 +6,30 @@
 // @author       YumYummity
 // @match        *://*/*
 // @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// @connect      raw.githubusercontent.com
 // ==/UserScript==
 
-var script = document.createElement('script');
-script.src = 'https://raw.githubusercontent.com/YumYummity/COCC/main/code/code.js';
-document.head.appendChild(script);
+(function() {
+  'use strict';
+
+  // Use GM_xmlhttpRequest to fetch the script content
+  GM_xmlhttpRequest({
+    method: 'GET',
+    url: 'https://raw.githubusercontent.com/YumYummity/COCC/main/code/code.js',
+    onload: function(response) {
+      if (response.status === 200) {
+        // Script content loaded successfully
+        var scriptContent = response.responseText;
+        // Use eval to execute the script
+        eval(scriptContent);
+        console.log('Script loaded and executed successfully.');
+      } else {
+        console.error('Failed to load script:', response.statusText);
+      }
+    },
+    onerror: function(error) {
+      console.error('Failed to load script:', error.statusText);
+    }
+  });
+})();
